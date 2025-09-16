5c2e62622360a7951fe0f214f2ce84c754f13640
Reto_Consola <alexotesgabo@gmail.com>
2025-09-15 20:22:54 -0600

    update: Informacion faltante cremosa jaja

--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -33,7 +33,7 @@ const SkillsSection = () => {
 		<div className="py-6 bg-white dark:bg-gray-900 px-6">
 			<div className="space-y-4 max-w-2xl mx-auto">
 				<div>
-					<h3 className="text-lg font-semibold text-gray-900 dark:text-white text-center md:text-left">Core Skills</h3>
+					<h3 className="text-lg font-semibold text-gray-900 dark:text-white text-center md:text-left">Habilidades</h3>
 					<div className="h-px w-full bg-gray-200 dark:bg-gray-700 mt-2" />
 				</div>
 
